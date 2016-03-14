9037353c49e77d7e19460fba6f4f7183137bbfb2
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-03-14 18:42:01 +0100

    Fix output, example

--- src/Example.purs
+++ src/Example.purs
@@ -4,12 +4,14 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Data.Maybe
+
 import Trace as Trace
 
-f :: Int -> Int -> Int
-f = Trace.trace "f" \a b -> a + b
+f :: Int -> Int -> Maybe Int -> Int
+f = Trace.trace "f" \a b c -> a + b
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   let g = f 8
-  log $ show $ g 4
+  log $ show $ g 3 Nothing
--- src/Trace.js
+++ src/Trace.js
@@ -2,23 +2,28 @@
 
 // module Trace
 
-function f(a) {
-  return function g(b, c) {
-    console.log("asd", a, b);
-  };
+var _intersperse = function(a, x) {
+  var b = [];
+
+  for (var i = 0; i < a.length - 1; i++) {
+    b.push(a[i]);
+    b.push(x);
+  }
+
+  b.push(a[a.length - 1]);
+  return b;
 }
 
 var _trace = function(f, str, traced_args) {
-  var that = this;
-
   if (typeof(f) === 'function') {
     return function() {
-      var r = f.apply(that, arguments);
-      return _trace(r, str, traced_args.concat(arguments));
+      var r = f.apply(this, arguments);
+      return _trace(r, str, traced_args.concat(Array.prototype.slice.call(arguments)));
     };
   }
   else {
-    console.log(str, traced_args);
+    console.log.apply(this,
+      [ str, "::" ].concat(_intersperse(traced_args, "->")).concat(["->", f]));
     return f;
   }
 }
