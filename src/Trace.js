426e0f7fdeaf74ac40e13670e972c04ce44b24c7
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-03-14 20:45:18 +0100

    Color, grouped args

--- src/Trace.js
+++ src/Trace.js
@@ -13,22 +13,37 @@ var _zip_const = function(a, x) {
   return b;
 }
 
-var _trace = function(f, str, traced_args) {
+var _get_name = function(o) {
+  if (o.constructor.name)
+    return o.constructor.name;
+  else
+    return o.toString();
+}
+
+var _trace = function(f, color, str, traced_args) {
   if (typeof(f) === 'function') {
     return function() {
       var r = f.apply(this, arguments);
-      return _trace(r, str, traced_args.concat(Array.prototype.slice.call(arguments)));
+      return _trace(r, color, str, traced_args.concat(Array.prototype.slice.call(arguments)));
     };
   }
   else {
-    console.log.apply(this,
-      [ str, "::" ].concat(_zip_const(traced_args, "->")).concat([f]));
+    console.groupCollapsed.call(console, "%c %s :: %s ➜ %s", 'color: '  + color, str, traced_args.map(_get_name).join(" ➜ "), f);
+
+    for (var i = 0; i < traced_args.length; i++)
+      console.log(traced_args[i]);
+    console.log(f);
+
+    console.groupEnd();
+
     return f;
   }
 }
 
-exports.trace = function(str) {
-  return function(f) {
-    return _trace(f, str, []);
-  }
+exports.traceColored = function(color) {
+  return function(str) {
+    return function(f) {
+      return _trace(f, color, str, []);
+    };
+  };
 }
--- src/Trace.purs
+++ src/Trace.purs
@@ -1,3 +1,18 @@
-module Trace (trace) where
+module Trace (trace, traceColored, orange, blue, green, red) where
 
-foreign import trace :: forall a. String -> a -> a
+foreign import traceColored :: forall a. String -> String -> a -> a
+
+orange :: String
+orange = "#fe6905"
+
+blue :: String
+blue = "#1e90ff"
+
+green :: String
+green = "#8dc63f"
+
+red :: String
+red = "#ae0202"
+
+trace :: forall a. String -> a -> a
+trace = traceColored "default"
